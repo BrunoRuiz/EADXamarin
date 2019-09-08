72b48937feab58650c7f7759e7bdfe97481773c6
Bruno Ruiz <brunoruiz07@gmail.com>
2019-09-08 15:36:35 -0300

    Sessao 10 - Style
    Aprendendo utilizar Style em nosso App.

---
+++ Solution6/Projeto6/Projeto6/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Projeto6.View;
+using Xamarin.Forms;
+
+namespace Projeto6
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+#if DEBUG
+            HotReloader.Current.Run(this);
+#endif
+
+            MainPage = new Page3();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
