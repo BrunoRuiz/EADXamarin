acef24b8fe686ba4edc1e81f3ae207a199e1e60a
Bruno Ruiz <brunoruiz07@gmail.com>
2019-09-08 15:34:51 -0300

    Sessao 09 - Controles Xamarin
    Aprendendo sobre os principais controles do Xamarin.Forms

---
+++ Solution5/Projeto5/Projeto5/View/WebViewPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projeto5.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebViewPage : ContentPage
+    {
+        public WebViewPage()
+        {
+            InitializeComponent();
+
+            var paginaHTML = new HtmlWebViewSource();
+            paginaHTML.Html = @"<html><body>
+                Esta Tela é um WebView Executando nosso HTML local.
+                </body></html>";
+
+            webView.Source = paginaHTML;
+        }
+    }
+}
