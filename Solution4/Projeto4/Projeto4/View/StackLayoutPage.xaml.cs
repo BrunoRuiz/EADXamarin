a0dbe40f636f5c571a63529d6737113c96825375
Bruno Ruiz <brunoruiz07@gmail.com>
2019-09-08 15:33:22 -0300

    Sessao 08 - Trabalhando com Layouts
    Aprendendo sobre os principais layouts do Xamarin.Forms

---
+++ Solution4/Projeto4/Projeto4/View/StackLayoutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Projeto4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class StackLayoutPage : ContentPage
+    {
+        public StackLayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
