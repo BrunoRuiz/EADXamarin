73dd7d5198620253eff1eb513d24253c69053560
Bruno Ruiz <brunoruiz07@gmail.com>
2019-09-08 12:09:53 -0300

    Sessao 04 - Entendendo o Xamarin.Forms
    Entendendo o Funcionando e estrutura do Xamarin.Forms

---
+++ Solution2/Projeto2/Projeto2/Classes/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto2.Classes
+{
+    public class MyClass
+    {
+        public static int Somar(int numero1, int numero2) => numero1 + numero2;
+    }
+}
---
+++ Solution2/Projeto2/Projeto2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Projeto2.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Projeto2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSomar_Clicked(object sender, EventArgs e)
+        {
+            int numero1 = int.Parse(edtNumero1.Text);
+            int numero2 = int.Parse(edtNumero2.Text);
+
+            int result = MyClass.Somar(numero1, numero2);
+
+            lblResultado.Text = "O Resultado é: " + result.ToString();
+        }
+    }
+}
