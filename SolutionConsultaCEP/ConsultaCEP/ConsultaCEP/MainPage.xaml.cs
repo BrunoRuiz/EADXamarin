8db499080e52b194633538fc46a8a5d7a5de834d
Bruno Ruiz <brunoruiz07@gmail.com>
2019-09-08 15:39:32 -0300

    Sessao 12 - Dicas
    Dicas e aprendendo a criar um App consultando uma API na web.

---
+++ SolutionConsultaCEP/ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ConsultaCEP.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsultaCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (edtCEP.Text != null && edtCEP.Text.Length == 8)
+            {
+                lblResultado.TextColor = Color.Blue;
+                lblResultado.Text = await ServiceCEP.BuscaCEP(edtCEP.Text);
+            }
+            else
+            {
+                lblResultado.TextColor = Color.Red;
+                lblResultado.Text = "Cep informado não encontrado ou inválido.";
+
+                //bool result = await DisplayAlert("ERRO", "Cep informado não encontrado ou inválido, Deseja buscar novamente?", "Sim", "Não");
+                
+                var result = await DisplayActionSheet("Teste do ActionSheet ?", "Cancelar", "Fechar", "Opção A", "Opção B", "Opção C");
+
+                lblResultado.Text = result.ToString();
+
+            }
+        }
+    }
+}
---
+++ SolutionConsultaCEP/ConsultaCEP/ConsultaCEP/Service/ServiceCEP.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaCEP.Service
+{
+    public class ServiceCEP
+    {
+        private static string UrlBase = "https://viacep.com.br/ws/{0}/json/";
+
+        public async static Task<string> BuscaCEP(string cep)
+        {
+
+            string Url = string.Format(UrlBase, cep);
+
+            HttpClient http = new HttpClient();
+            string json = await http.GetStringAsync(Url);
+
+            dynamic objetoCEP = JsonConvert.DeserializeObject<dynamic>(json);
+
+            string result = string.Format("CEP: {0} \n Endereço: {1} \n Bairro: {2} \n Cidade: {3} \n UF: {4}",
+                objetoCEP.cep, objetoCEP.logradouro, objetoCEP.bairro, objetoCEP.localidade, objetoCEP.uf);
+
+            return result;
+
+        }
+    }
+}
