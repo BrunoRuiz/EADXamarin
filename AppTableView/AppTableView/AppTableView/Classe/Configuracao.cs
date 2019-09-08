d07d2ba075845437c891d63763e55d97d6f3bae0
Bruno Ruiz <brunoruiz07@gmail.com>
2019-09-08 15:29:07 -0300

    Sessao 06 - TableView
    Aprendendo sobre TableView

---
+++ AppTableView/AppTableView/AppTableView/Classe/Configuracao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTableView.Classe
+{
+    public class Configuracao
+    {
+        public bool SyncAutomatico { get; set; }
+        public string NomeArquivoBackup { get; set; }
+        public bool NoficationEmail { get; set; }
+        public bool NotificationPush { get; set; }
+        public string Observacao { get; set; }
+
+        public Configuracao()
+        {
+            NoficationEmail = false;
+            NomeArquivoBackup = "ArquivoTeste";
+            NotificationPush = true;
+            Observacao = "Aplicativo de teste sobre TableView - Criando nossa tela de Configuração.";
+            SyncAutomatico = true;
+        }
+
+    }
+}
